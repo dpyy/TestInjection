2c4fe38e1057332b0b020514df5eb3ce3a574379
Gary <gary.fung@me.com>
2023-02-20 21:48:41 -0500

    Initial Commit

---
+++ TestUI/TestUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUIApp.swift
+//  TestUI
+//
+//  Created by Gary Fung on 2023-02-20.
+//
+
+import SwiftUI
+
+@main
+struct TestUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
